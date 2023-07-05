b550b3bbe0ed0f0413a1c2b68fa77c2697e5848d
bishal-dd <dhakalbishal930@gmail.com>
2023-07-05 14:00:11 +0600

    signin and signout complete

---
+++ app/controllers/concerns/common.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Common
+  def render_resources(resource)
+    if resource.errors.empty?
+      render json: { status: "success", data: resource }
+    else
+      render json: { status: "error", errors: resource.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+end
